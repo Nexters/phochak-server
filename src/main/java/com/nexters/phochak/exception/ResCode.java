a199077c757af222369165b8ca1a7f9b804ed840
JinuCheon <woojin8787@gmail.com>
2023-01-17 21:47:21 +0900

    feat: 예시 Response Code 추가

--- src/main/java/com/nexters/phochak/exception/ResCode.java
+++ src/main/java/com/nexters/phochak/exception/ResCode.java
@@ -2,6 +2,20 @@ package com.nexters.phochak.exception;
 
 public enum ResCode {
 
+    // P0xx: 정상 처리
+    OK("P000", "정상 처리"),
+
+    //P1xx: 서버 예외
+    INTERNAL_SERVER_ERROR("P100", "서버 에러 발생");
+
+    //P2xx: 인증 예외
+
+    //P3xx: 유저 예외
+
+    //P4xx: 게시글 예외
+
+    //P5xx: 파일 예외
+
     private final String code;
     private final String message;
 
