3cc5ef30e74e9b7a7cf666aa00652d4aaad24fca
seyoung755 <seyoung7555@naver.com>
2023-01-21 17:47:08 +0900

    feat: OAuth 로그인 구현
    - 유저 엔티티 속성 추가

--- src/main/java/com/nexters/phochak/domain/User.java
+++ src/main/java/com/nexters/phochak/domain/User.java
@@ -1,12 +1,43 @@
 package com.nexters.phochak.domain;
 
-import javax.persistence.*;
+import com.nexters.phochak.specification.OAuthProviderEnum;
+import lombok.Builder;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class User {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    @Column(name="USER_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private OAuthProviderEnum provider;
+
+    @Column(nullable = false, unique = true)
+    private String providerId;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    private String profileImgUrl;
+
+    public User() {
+    }
+
+    @Builder
+    public User(OAuthProviderEnum provider, String providerId, String nickname, String profileImgUrl) {
+        this.provider = provider;
+        this.providerId = providerId;
+        this.nickname = nickname;
+        this.profileImgUrl = profileImgUrl;
+    }
 }
