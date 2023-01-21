903159e82c6567eb8b1f12025039136244cebdb7
seyoung755 <seyoung7555@naver.com>
2023-01-21 17:50:00 +0900

    feat: OAuth 로그인 구현
    - 유저 가입용 API 구성

--- src/main/java/com/nexters/phochak/controller/UserController.java
+++ src/main/java/com/nexters/phochak/controller/UserController.java
@@ -1,12 +1,9 @@
 package com.nexters.phochak.controller;
 
-import com.nexters.phochak.dto.JoinRequestDto;
 import com.nexters.phochak.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/v1")
 @RestController
-public class LoginController {
+public class UserController {
     private final UserService userService;
 
     @GetMapping("login/{provider}")
     public void login(@PathVariable String provider, @RequestParam String code) {
         userService.login(provider, code);
-    }
-
-    @PostMapping("join/{provider}")
-    public void join(@PathVariable String provider, @RequestBody JoinRequestDto joinRequestDto) {
-        userService.join(provider, joinRequestDto);
     }
 }
