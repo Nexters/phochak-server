0171e608c9f969b077ff61283d2c727025e383e1
seyoung755 <seyoung7555@naver.com>
2023-01-23 18:11:56 +0900

    feat: Controller에 Validation 로직 추가
    - exception handler 로깅 강화
    - ModelAttribute로 들어오는 input값 validation 실패 시 처리할 ExceptionHandler 추가

--- src/main/java/com/nexters/phochak/exception/CustomExceptionHandler.java
+++ src/main/java/com/nexters/phochak/exception/CustomExceptionHandler.java
@@ -4,16 +4,22 @@ import com.nexters.phochak.dto.ExceptionResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Slf4j
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
     @ExceptionHandler(PhochakException.class)
-    protected ResponseEntity<ExceptionResponseDto> handlePhochakCustomException(PhochakException e) {
-        log.info(e.getMessage());
+    protected ResponseEntity<ExceptionResponseDto> handlePhochakCustomException(PhochakException e, HttpServletRequest request) {
+        log.warn("[PhochakException 발생] request url: {}", request.getRequestURI(), e);
         return ResponseEntity.ok().body(ExceptionResponseDto.builder()
                 .resCode(e.getResCode())
                 .customResMessage(e.getCustomResMessage())
@@ -21,11 +27,37 @@ public class CustomExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ExceptionResponseDto> handleInternalErrorException(Exception e) {
-        log.error("Internal Error Message: {}", e.getMessage(), e);
+    protected ResponseEntity<ExceptionResponseDto> handleInternalErrorException(Exception e, HttpServletRequest request) {
+        log.error("[Internal Error Message] request url: {}", request.getRequestURI(), e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ExceptionResponseDto.builder()
                 .resCode(ResCode.INTERNAL_SERVER_ERROR)
                 .build());
     }
 
+    @ExceptionHandler(BindException.class)
+    protected ResponseEntity<ExceptionResponseDto> handleBindException(BindException e, HttpServletRequest request) {
+        log.warn("[BindException 발생] request url: {}", request.getRequestURI(), e);
+        List<ObjectError> allErrors = e.getBindingResult().getAllErrors();
+
+        StringBuilder stringBuilder = getResponseStringBuilder(allErrors);
+
+        return ResponseEntity.ok(ExceptionResponseDto.builder()
+                .resCode(ResCode.INVALID_INPUT)
+                .customResMessage(stringBuilder.toString())
+                .build());
+    }
+
+    private static StringBuilder getResponseStringBuilder(Iterable<ObjectError> allErrors) {
+        StringBuilder stringBuilder = new StringBuilder();
+        allErrors.forEach(error -> stringBuilder
+                            .append("['")
+                            .append(((FieldError) error).getField())
+                            .append("' is '")
+                            .append(((FieldError) error).getRejectedValue())
+                            .append("' :: ")
+                            .append(error.getDefaultMessage())
+                            .append("] ")
+                );
+        return stringBuilder;
+    }
 }
