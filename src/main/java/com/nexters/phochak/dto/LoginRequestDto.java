c485e729cb812e0b05684ecc97e2585f2953a37f
seyoung755 <seyoung7555@naver.com>
2023-01-23 18:11:26 +0900

    feat: Controller에 Validation 로직 추가
    - controller 입력값에 valid 로직 추가

---
+++ src/main/java/com/nexters/phochak/dto/LoginRequestDto.java
@@ -0,0 +1,13 @@
+package com.nexters.phochak.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@AllArgsConstructor
+public class LoginRequestDto {
+    @NotBlank
+    private String code;
+}
