70decc766e7e411d563d86258c852c424881245d
seyoung755 <seyoung7555@naver.com>
2023-01-24 23:59:11 +0900

    feat: JWT 토큰 생성 및 검증 로직 추가
    - api 문서 작성

--- src/test/java/com/nexters/phochak/controller/UserControllerTest.java
+++ src/test/java/com/nexters/phochak/controller/UserControllerTest.java
@@ -1,7 +1,10 @@
 package com.nexters.phochak.controller;
 
 import com.nexters.phochak.docs.RestDocs;
+import com.nexters.phochak.dto.TokenDto;
+import com.nexters.phochak.dto.response.LoginResponseDto;
 import com.nexters.phochak.service.UserService;
+import com.nexters.phochak.service.impl.JwtTokenServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,17 +12,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
@@ -30,10 +35,10 @@ class UserControllerTest extends RestDocs {
 
     @Mock
     UserService userService;
-
+    @Mock
+    JwtTokenServiceImpl jwtTokenService;
     @InjectMocks
     UserController userController;
-
     MockMvc mockMvc;
 
     @BeforeEach
@@ -47,22 +52,42 @@ class UserControllerTest extends RestDocs {
         String provider = "kakao";
         String code = "testCode";
 
+        LoginResponseDto response = LoginResponseDto.builder()
+                .tokenType(TokenDto.TOKEN_TYPE)
+                .accessToken("access token")
+                .expiresIn("access token lifetime")
+                .refreshToken("refresh token")
+                .refreshTokenExpiresIn("refresh token lifetime")
+                .build();
+
+        when(jwtTokenService.createLoginResponse(any())).thenReturn(response);
+
         mockMvc.perform(
-                RestDocumentationRequestBuilders
-                        .get("/v1/user/login/{provider}", provider)
-                        .param("code", code)
-        )
-        .andExpect(status().isOk())
-        .andDo(document("user/login",
-                preprocessRequest(prettyPrint()),
-                preprocessResponse(prettyPrint()),
-                pathParameters(
-                        parameterWithName("provider").description("OAuth 서비스 이름")
-                ),
-                requestParameters(
-                        parameterWithName("code").description("Authorization code")
-                ))
-                // TODO: 로그인 완성 후 응답 mocking 필요
-        );
+                        RestDocumentationRequestBuilders
+                                .get("/v1/user/login/{provider}", provider)
+                                .param("code", code)
+                )
+                .andExpect(status().isOk())
+                .andDo(document("user/login",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                pathParameters(
+                                        parameterWithName("provider").description("OAuth 서비스 이름(ex. kakao, apple, naver")
+                                ),
+                                requestParameters(
+                                        parameterWithName("code").description("Authorization code")
+                                ),
+                                responseFields(
+                                        fieldWithPath("tokenType").type(JsonFieldType.STRING).description("토큰 타입"),
+                                        fieldWithPath("accessToken").type(JsonFieldType.STRING).description("access token"),
+                                        fieldWithPath("expiresIn").type(JsonFieldType.STRING).description("access token 유효기간(ms)"),
+                                        fieldWithPath("refreshToken").type(JsonFieldType.STRING).description("refresh token"),
+                                        fieldWithPath("refreshTokenExpiresIn").type(JsonFieldType.STRING).description("refresh token 유효기간(ms)")
+                                )
+                        // TODO: 로그인 완성 후 응답 mocking 필요
+                ));
     }
+
+
+    // TODO: provider enum 없을 때 예외처리 + 문서 작성
 }
