6386c85a6870fa8bbc052d51bbad804ef2a93392
seyoung755 <seyoung7555@naver.com>
2023-01-25 01:05:22 +0900

    feat: JWT 토큰 생성 및 검증 로직 추가
    - ResCode 추가

--- src/main/java/com/nexters/phochak/exception/ResCode.java
+++ src/main/java/com/nexters/phochak/exception/ResCode.java
@@ -9,7 +9,10 @@ public enum ResCode {
     INTERNAL_SERVER_ERROR("P100", "서버 에러 발생"),
 
     //P2xx: 인증 예외
-    INVALID_INPUT("P200", "올바르지 않은 입력값"),
+    INVALID_INPUT("P200", "올바르지 않은 입력값입니다"),
+    TOKEN_NOT_FOUND("P201", "토큰을 찾을 수 없습니다(로그인 되지 않은 사용자입니다.)"),
+    INVALID_TOKEN("P202", "올바르지 않은 토큰입니다"),
+    EXPIRED_TOKEN("P203", "만료된 토큰입니다"),
 
     //P3xx: 유저 예외
     NOT_FOUND_USER("P300", "존재하지 않는 유저입니다");
