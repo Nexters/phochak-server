4622a0dda8e1c7451fcf70b2d059e942233b8934
Seyeong Kim <seyoung7555@naver.com>
2023-01-24 18:40:18 +0900

    feat: Controller에 Validation 로직 추가 (#18)
    * feat: Controller에 Validation 로직 추가
    
    - validation 의존성 추가
    - validation 관련 응답코드 추가
    - controller 입력값에 valid 로직 추가
    - exception handler 로깅 강화
    - ModelAttribute로 들어오는 input값 validation 실패 시 처리할 ExceptionHandler 추가

--- src/main/java/com/nexters/phochak/controller/UserController.java
+++ src/main/java/com/nexters/phochak/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.nexters.phochak.controller;
 
+import com.nexters.phochak.dto.LoginRequestDto;
 import com.nexters.phochak.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/v1/user")
 @RestController
@@ -15,7 +19,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("login/{provider}")
-    public void login(@PathVariable String provider, @RequestParam String code) {
-        userService.login(provider, code);
+    public void login(@PathVariable String provider, @Valid LoginRequestDto requestDto) {
+        userService.login(provider, requestDto.getCode());
     }
 }
