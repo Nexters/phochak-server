a30d943ce0917be47f45805046041a182abb6ac5
seyoung755 <seyoung7555@naver.com>
2023-02-01 01:12:08 +0900

    feat: 포스트 조회 pagination 구현
    - 포스트 페이징 조회 서비스 구현중

--- src/main/java/com/nexters/phochak/service/PostService.java
+++ src/main/java/com/nexters/phochak/service/PostService.java
@@ -1,8 +1,21 @@
 package com.nexters.phochak.service;
 
 import com.nexters.phochak.dto.request.PostCreateRequestDto;
+import com.nexters.phochak.dto.response.PostPageResponseDto;
+
+import java.util.List;
 
 public interface PostService {
 
     void create(Long userId, PostCreateRequestDto postCreateRequestDto);
+
+    /**
+     * 다음 커서로부터 Page size만큼의 page를 가져온다.
+     * @param lastId
+     * @param pageSize
+     * @param postSortCriteria
+     * @param lastCriteriaValue
+     * @return
+     */
+    List<PostPageResponseDto> getNextCursorPage(Long lastId, Long pageSize, String postSortCriteria, Long lastCriteriaValue);
 }
--- src/main/java/com/nexters/phochak/service/impl/PostServiceImpl.java
+++ src/main/java/com/nexters/phochak/service/impl/PostServiceImpl.java
@@ -4,19 +4,27 @@ import com.nexters.phochak.domain.Post;
 import com.nexters.phochak.domain.Shorts;
 import com.nexters.phochak.domain.User;
 import com.nexters.phochak.dto.request.PostCreateRequestDto;
+import com.nexters.phochak.dto.response.PostPageResponseDto;
+import com.nexters.phochak.exception.PhochakException;
+import com.nexters.phochak.exception.ResCode;
 import com.nexters.phochak.repository.PostRepository;
 import com.nexters.phochak.repository.UserRepository;
 import com.nexters.phochak.service.HashtagService;
 import com.nexters.phochak.service.PostService;
 import com.nexters.phochak.specification.PostCategoryEnum;
+import com.nexters.phochak.specification.PostSortCriteria;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
 
+    public static final long DEFAULT_PAGE_SIZE = 5;
     private final UserRepository userRepository;
     private final PostRepository postRepository;
     private final ShortsService shortsService;
@@ -36,4 +44,25 @@ public class PostServiceImpl implements PostService {
         postRepository.save(post);
     }
 
+    @Override
+    @Transactional(readOnly = true)
+    public List<PostPageResponseDto> getNextCursorPage(Long lastId, Long pageSize, String postSortCriteria, Long lastCriteriaValue) {
+        if (Objects.isNull(pageSize)) {
+            pageSize = DEFAULT_PAGE_SIZE;
+        }
+
+        validatePostSortCriteriaAndCriteriaValue(postSortCriteria, lastCriteriaValue);
+
+        postRepository.findNextPageByCursor(lastId, pageSize, postSortCriteria, lastCriteriaValue);
+
+        return null;
+    }
+
+    private static void validatePostSortCriteriaAndCriteriaValue(String postSortCriteria, Long lastCriteriaValue) {
+        PostSortCriteria postSortCriteriaEnum = PostSortCriteria.nameOf(postSortCriteria);
+
+        if (Objects.isNull(lastCriteriaValue) && postSortCriteriaEnum != PostSortCriteria.LATEST) {
+            throw new PhochakException(ResCode.NOT_FOUND_LAST_CRITERIA_VALUE);
+        }
+    }
 }
