fa8d021e81bdc9983a650985ac4e82816b234b96
seyoung755 <seyoung7555@naver.com>
2023-02-06 01:29:57 +0900

    feat: 포스트 조회 pagination 구현
    - 테스트 포매팅 수정

--- src/test/java/com/nexters/phochak/controller/PostControllerTest.java
+++ src/test/java/com/nexters/phochak/controller/PostControllerTest.java
@@ -5,8 +5,11 @@ import com.nexters.phochak.dto.TokenDto;
 import com.nexters.phochak.repository.UserRepository;
 import com.nexters.phochak.service.impl.JwtTokenServiceImpl;
 import com.nexters.phochak.specification.OAuthProviderEnum;
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
@@ -14,7 +17,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.File;
@@ -30,26 +32,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
-@ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class PostControllerTest {
-
-    @Autowired UserRepository userRepository;
-    @Autowired JwtTokenServiceImpl jwtTokenService;
-    @Autowired MockMvc mockMvc;
-    @Value("${app.resource.local.shorts}") String shortsPath;
+class PostControllerTest {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    JwtTokenServiceImpl jwtTokenService;
+    @Autowired
+    MockMvc mockMvc;
+    @Value("${app.resource.local.shorts}")
+    String shortsPath;
 
     static String testToken;
 
     @BeforeAll
     void setUp() {
         User user = User.builder()
-                        .providerId("1234")
-                        .provider(OAuthProviderEnum.KAKAO)
-                        .nickname("nickname")
-                        .profileImgUrl(null)
-                        .build();
+                .providerId("1234")
+                .provider(OAuthProviderEnum.KAKAO)
+                .nickname("nickname")
+                .profileImgUrl(null)
+                .build();
         userRepository.save(user);
         TokenDto tokenDto = jwtTokenService.generateAccessToken(user.getId());
         testToken = TokenDto.TOKEN_TYPE + " " + tokenDto.getTokenString();
@@ -58,12 +62,12 @@ public class PostControllerTest {
     @AfterAll
     void removeTestVideo() {
         File deleteFolder = new File(shortsPath);
-        if(deleteFolder.exists()){
+        if (deleteFolder.exists()) {
             File[] deleteFolderList = deleteFolder.listFiles();
             for (File file : deleteFolderList) {
                 file.delete();
             }
-            if(deleteFolderList.length == 0 && deleteFolder.isDirectory()){
+            if (deleteFolderList.length == 0 && deleteFolder.isDirectory()) {
                 deleteFolder.delete();
             }
         }
@@ -81,12 +85,12 @@ public class PostControllerTest {
 
         // when, then
         mockMvc.perform(multipart("/v1/post")
-                .file(testVideo)
-                .param("postCategory", "RESTAURANT")
-                .param("hashtags", "[\"해시태그1\", \"해시태그2\", \"해시태그3\"))]")
-                .header(AUTHORIZATION_HEADER, testToken)
-        ).andExpect(status().isOk())
-        .andExpect(jsonPath("$.resCode").value(OK.getCode()));
+                        .file(testVideo)
+                        .param("postCategory", "RESTAURANT")
+                        .param("hashtags", "[\"해시태그1\", \"해시태그2\", \"해시태그3\"))]")
+                        .header(AUTHORIZATION_HEADER, testToken)
+                ).andExpect(status().isOk())
+                .andExpect(jsonPath("$.resCode").value(OK.getCode()));
     }
 
     @Test
@@ -153,7 +157,7 @@ public class PostControllerTest {
                 new FileInputStream("app-resource/test/dummy/test.mov"));
 
         StringBuilder hashtagStringList = new StringBuilder("[");
-        for(int i=0;i<31;i++) {
+        for (int i = 0; i < 31; i++) {
             hashtagStringList.append("\"해시태그").append(i).append("\",");
         }
         hashtagStringList.deleteCharAt(hashtagStringList.length() - 1);
