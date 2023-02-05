bceb9c2c9c33658d02fa69cf768d9244c11a34d0
seyoung755 <seyoung7555@naver.com>
2023-02-06 01:29:21 +0900

    feat: 포스트 조회 pagination 구현
    - 포스트 조회 시 응답 dto 생성

--- src/main/java/com/nexters/phochak/dto/response/PostPageResponseDto.java
+++ src/main/java/com/nexters/phochak/dto/response/PostPageResponseDto.java
@@ -1,5 +1,35 @@
 package com.nexters.phochak.dto.response;
 
-public class PostPageResponseDto {
+import com.nexters.phochak.domain.Post;
+import com.nexters.phochak.domain.Shorts;
+import com.nexters.phochak.domain.User;
+import com.nexters.phochak.specification.PostCategoryEnum;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class PostPageResponseDto {
+    private long id;
+    private long userId;
+    private User user;
+    private Shorts shorts;
+    private long view;
+    private PostCategoryEnum postCategoryEnum;
+    private long phochakCount;
+
+    public static PostPageResponseDto from(Post post) {
+        return PostPageResponseDto.builder()
+                .id(post.getId())
+                .user(post.getUser())
+                .userId(post.getUser().getId())
+                .shorts(post.getShorts())
+                .view(post.getView())
+                .postCategoryEnum(post.getPostCategory())
+                .phochakCount(post.getPhochaks().size())
+                .build();
+    }
 }
