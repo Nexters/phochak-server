bd1f8f4e5b64463f2357aafeb7e3443cb69b2198
seyoung755 <seyoung7555@naver.com>
2023-02-07 23:34:12 +0900

    feat: 포스트 조회 pagination 구현
    - 테스트용 웹 카카오 로그인 API 생성

--- src/main/java/com/nexters/phochak/controller/UserController.java
+++ src/main/java/com/nexters/phochak/controller/UserController.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -25,6 +26,13 @@ public class UserController {
     @GetMapping("login/{provider}")
     public CommonResponse<LoginResponseDto> login(@PathVariable String provider, @Valid LoginRequestDto requestDto) {
         Long loginUserId = userService.login(provider, requestDto.getToken());
+        return new CommonResponse<>(jwtTokenService.createLoginResponse(loginUserId));
+    }
+
+    // test(web oauth) 용 api, provider를 kakao_test 로 명시
+    @GetMapping("test/login/{provider}")
+    public CommonResponse<LoginResponseDto> login(@PathVariable String provider, @RequestParam String code) {
+        Long loginUserId = userService.login(provider, code);
         return new CommonResponse<>(jwtTokenService.createLoginResponse(loginUserId));
     }
 }
