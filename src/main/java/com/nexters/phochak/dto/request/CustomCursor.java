90f2c25ced3a987c4ec92be8b5a78a0eb96c3f96
seyoung755 <seyoung7555@naver.com>
2023-02-07 23:10:18 +0900

    feat: 포스트 조회 pagination 구현
    - parameter 관련 수정사항 반영

--- src/main/java/com/nexters/phochak/dto/request/CustomCursor.java
+++ src/main/java/com/nexters/phochak/dto/request/CustomCursor.java
@@ -2,7 +2,7 @@ package com.nexters.phochak.dto.request;
 
 import com.nexters.phochak.exception.PhochakException;
 import com.nexters.phochak.exception.ResCode;
-import com.nexters.phochak.specification.PostSortCriteria;
+import com.nexters.phochak.specification.PostSortOption;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -23,33 +23,35 @@ public class CustomCursor {
     private Long lastId;
     private Integer pageSize;
     @NotNull
-    private PostSortCriteria postSortCriteria;
-    private Integer lastCriteriaValue;
-    private Boolean isInitialRequest;
+    private PostSortOption sortOption;
+    private Integer sortValue;
 
     @Builder
-    public CustomCursor(Long lastId, Integer pageSize, PostSortCriteria postSortCriteria, Integer lastCriteriaValue, Boolean isInitialRequest) {
+    public CustomCursor(Long lastId, Integer pageSize, PostSortOption sortOption, Integer sortValue) {
         this.lastId = lastId;
         this.pageSize = Objects.isNull(pageSize) ? DEFAULT_PAGE_SIZE : pageSize;
-        this.postSortCriteria = postSortCriteria;
-        this.lastCriteriaValue = lastCriteriaValue;
-        this.isInitialRequest = Objects.isNull(isInitialRequest) ? Boolean.FALSE : isInitialRequest;
+        this.sortOption = sortOption;
+        this.sortValue = sortValue;
 
-        if (Boolean.TRUE.equals(isInitialRequest)) {
+        // 첫 요청 시 lastId, sortValue는 null로, sortOption만 받음
+        if (Objects.isNull(lastId) && Objects.isNull(sortValue)) {
+            if (Objects.isNull(sortOption)) {
+                throw new PhochakException(ResCode.NOT_FOUND_SORT_OPTION);
+            }
             this.lastId = Long.MAX_VALUE;
-            this.lastCriteriaValue = Integer.MAX_VALUE;
+            this.sortValue = Integer.MAX_VALUE;
         } else {
-            if (Objects.isNull(lastCriteriaValue) && postSortCriteria != PostSortCriteria.LATEST) {
-                throw new PhochakException(ResCode.NOT_FOUND_LAST_CRITERIA_VALUE);
+            if (Objects.isNull(sortValue) && sortOption != PostSortOption.LATEST) {
+                throw new PhochakException(ResCode.NOT_FOUND_SORT_VALUE);
             }
         }
     }
 
     public String createCursorString() {
-        if (Objects.isNull(lastCriteriaValue)) {
+        if (Objects.isNull(sortValue)) {
             return null;
         }
 
-        return String.format(INTEGER_STRING_FORMATTER, lastCriteriaValue) + String.format(LONG_STRING_FORMATTER, lastId);
+        return String.format(INTEGER_STRING_FORMATTER, sortValue) + String.format(LONG_STRING_FORMATTER, lastId);
     }
 }
