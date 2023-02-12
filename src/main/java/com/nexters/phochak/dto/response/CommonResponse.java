1ba520d400d6701098be8a13112525f2173a07a2
Seyeong Kim <seyoung7555@naver.com>
2023-02-13 00:42:59 +0900

    refactor: 예외 응답시에도 data 필드 넣어주기  (#69)
    * refactor: 예외 응답시에도 data 필드 넣어주기
    
    - PhochakException 에 @Getter 추가
    - ExceptionHandler에서 CommonResponse 응답하도록 수정

--- src/main/java/com/nexters/phochak/dto/response/CommonResponse.java
+++ src/main/java/com/nexters/phochak/dto/response/CommonResponse.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 @Getter
 public class CommonResponse<T> {
     private StatusResponse status = new StatusResponse(ResCode.OK);
-
     private final T data;
 
     public CommonResponse() {
@@ -17,5 +16,15 @@ public class CommonResponse<T> {
 
     public CommonResponse(T data) {
         this.data = data;
+    }
+
+    public CommonResponse(ResCode resCode) {
+        this();
+        this.status = new StatusResponse(resCode);
+    }
+
+    public CommonResponse(String resCode, String resMessage) {
+        this();
+        this.status = new StatusResponse(resCode, resMessage);
     }
 }
