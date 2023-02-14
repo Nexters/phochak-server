685d973d54bfaac4fa4c3bd7fd2325fc46832b20
JinuCheon <woojin8787@gmail.com>
2023-02-14 19:04:48 +0900

    feat: redis CRD 기능 구현

--- src/main/java/com/nexters/phochak/repository/RefreshTokenRepository.java
+++ src/main/java/com/nexters/phochak/repository/RefreshTokenRepository.java
@@ -1,13 +1,10 @@
 package com.nexters.phochak.repository;
 
-import com.nexters.phochak.domain.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface RefreshTokenRepository {
 
     void saveWithAccessToken(String refreshToken, String accessToken);
 
     String findAccessToken(String refreshToken);
 
-    String expire(String refreshToken);
+    Boolean expire(String refreshToken);
 }
