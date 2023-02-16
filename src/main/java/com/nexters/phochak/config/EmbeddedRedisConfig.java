b645a7e82d26c08ce55d1499415f6b748235bc5e
JinuCheon <woojin8787@gmail.com>
2023-02-16 13:35:14 +0900

    chore: embedded Redis 사용시 기존 포트가 사용중이면 다른 포트 사용

--- src/main/java/com/nexters/phochak/config/EmbeddedRedisConfig.java
+++ src/main/java/com/nexters/phochak/config/EmbeddedRedisConfig.java
@@ -4,10 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.util.StringUtils;
 import redis.embedded.RedisServer;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @Slf4j
 @Profile("test")
@@ -15,14 +19,14 @@ import javax.annotation.PreDestroy;
 public class EmbeddedRedisConfig {
 
     @Value("${spring.redis.port}")
-    private int testRedisPort;
+    private int redisPort;
 
     private RedisServer redisServer;
 
     @PostConstruct
-    public void redisServer() {
-        System.out.println("testRedisPort = " + testRedisPort);
-        redisServer = new RedisServer(testRedisPort);
+    public void redisServer() throws IOException {
+        int port = isRedisRunning()? findAvailablePort() : redisPort;
+        redisServer = new RedisServer(port);
         redisServer.start();
     }
 
@@ -31,5 +35,44 @@ public class EmbeddedRedisConfig {
         if (redisServer != null) {
             redisServer.stop();
         }
+    }
+
+    private boolean isRedisRunning() throws IOException {
+        return isRunning(executeGrepProcessCommand(redisPort));
+    }
+
+    /**
+     * 기본 포트가 사용중일 경우 사용중이지 않은 다른 포트 검색
+     */
+    public int findAvailablePort() throws IOException {
+        for (int port = 10000; port <= 65535; port++) {
+            Process process = executeGrepProcessCommand(port);
+            if (!isRunning(process)) {
+                return port;
+            }
+        }
+        throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
+    }
+
+    private Process executeGrepProcessCommand(int port) throws IOException {
+        String command = String.format("netstat -nat | grep LISTEN|grep %d", port);
+        String[] shell = {"/bin/sh", "-c", command};
+        return Runtime.getRuntime().exec(shell);
+    }
+
+    private boolean isRunning(Process process) {
+        String line;
+        StringBuilder pidInfo = new StringBuilder();
+
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+
+            while ((line = input.readLine()) != null) {
+                pidInfo.append(line);
+            }
+
+        } catch (Exception e) {
+        }
+
+        return !StringUtils.isEmpty(pidInfo.toString());
     }
 }
