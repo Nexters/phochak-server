7ba787c5c2e6f645130f070603b81ecad3116b73
JinuCheon <woojin8787@gmail.com>
2023-02-17 03:05:17 +0900

    test: 토큰 재발급 실패 케이스 추가

--- src/test/java/com/nexters/phochak/integration/AuthIntegrationTest.java
+++ src/test/java/com/nexters/phochak/integration/AuthIntegrationTest.java
@@ -6,12 +6,10 @@ import com.nexters.phochak.docs.RestDocs;
 import com.nexters.phochak.domain.User;
 import com.nexters.phochak.dto.TokenDto;
 import com.nexters.phochak.dto.request.ReissueTokenRequestDto;
-import com.nexters.phochak.exception.PhochakException;
-import com.nexters.phochak.exception.ResCode;
+import com.nexters.phochak.exception.CustomExceptionHandler;
 import com.nexters.phochak.repository.RefreshTokenRepository;
 import com.nexters.phochak.repository.UserRepository;
 import com.nexters.phochak.service.JwtTokenService;
-import com.nexters.phochak.service.impl.JwtTokenServiceImpl;
 import com.nexters.phochak.specification.OAuthProviderEnum;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -27,19 +25,18 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static com.nexters.phochak.auth.aspect.AuthAspect.AUTHORIZATION_HEADER;
-import static com.nexters.phochak.exception.ResCode.INVALID_INPUT;
+import static com.nexters.phochak.exception.ResCode.EXPIRED_TOKEN;
+import static com.nexters.phochak.exception.ResCode.INVALID_TOKEN;
 import static com.nexters.phochak.exception.ResCode.OK;
-import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
-import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -56,12 +53,13 @@ public class AuthIntegrationTest extends RestDocs {
     @Autowired ObjectMapper objectMapper;
     @Autowired RefreshTokenRepository refreshTokenRepository;
     MockMvc mockMvc;
-    static String testToken;
     static Long globalUserId;
 
     @BeforeEach
     void setUp(RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = getMockMvcBuilder(restDocumentation, userController).build();
+        this.mockMvc = getMockMvcBuilder(restDocumentation, userController)
+                .setControllerAdvice(CustomExceptionHandler.class)
+                .build();
         User user = User.builder()
                 .providerId("1234")
                 .provider(OAuthProviderEnum.KAKAO)
@@ -78,7 +76,7 @@ public class AuthIntegrationTest extends RestDocs {
 
     @Test
     @DisplayName("토큰 재발급 API - 재발급 성공")
-    void addPhochak() throws Exception {
+    void reissueToken() throws Exception {
         //given
         TokenDto currentAT = jwtTokenService.generateToken(globalUserId, -1000L);
         TokenDto currentRT = jwtTokenService.generateToken(globalUserId, 9999999999L);
@@ -111,6 +109,75 @@ public class AuthIntegrationTest extends RestDocs {
                             fieldWithPath("data.refreshTokenExpiresIn").type(JsonFieldType.STRING).description("refresh token 유효기간(ms)")
                     )
             ));
+    }
+
+    @Test
+    @DisplayName("토큰 재발급 API - Refresh Token이 만료된 경우, Expired Token 예외가 발생한다")
+    void reissueToken_RTExpired_InvalidToken() throws Exception {
+        //given
+        TokenDto currentAT = jwtTokenService.generateToken(globalUserId, -2000L);
+        TokenDto currentRT = jwtTokenService.generateToken(globalUserId, -1000L);
+
+        refreshTokenRepository.saveWithAccessToken(currentRT.getTokenString(), currentAT.getTokenString());
+
+        ReissueTokenRequestDto body = new ReissueTokenRequestDto(
+                createTokenStringForResponse(currentAT),
+                createTokenStringForResponse(currentRT));
+
+        //when, then
+        mockMvc.perform(post("/v1/user/reissue-token")
+                        .content(objectMapper.writeValueAsString(body))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status.resCode").value(EXPIRED_TOKEN.getCode()));
+    }
+
+    @Test
+    @DisplayName("토큰 재발급 API - Access Token이 아직 만료되지 않은 경우, 탈취로 간주하고 Invalid Token 예외가 발생한다")
+    void reissueToken_ATNotExpired_InvalidToken() throws Exception {
+        //given
+        TokenDto currentAT = jwtTokenService.generateToken(globalUserId, 1000000000L);
+        TokenDto currentRT = jwtTokenService.generateToken(globalUserId, 9999999999L);
+
+        refreshTokenRepository.saveWithAccessToken(currentRT.getTokenString(), currentAT.getTokenString());
+
+        ReissueTokenRequestDto body = new ReissueTokenRequestDto(
+                createTokenStringForResponse(currentAT),
+                createTokenStringForResponse(currentRT));
+
+        //when, then
+        mockMvc.perform(post("/v1/user/reissue-token")
+                        .content(objectMapper.writeValueAsString(body))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status.resCode").value(INVALID_TOKEN.getCode()));
+    }
+
+    @Test
+    @DisplayName("토큰 재발급 API - AT와 RT가 매치되지 않는 경우, 탈취로 간주하고 Invalid Token 예외가 발생한다")
+    void reissueToken_ATRTNotMatched_InvalidToken() throws Exception {
+        //given
+        TokenDto currentAT1 = jwtTokenService.generateToken(globalUserId, 1000000000L);
+        TokenDto currentRT1 = jwtTokenService.generateToken(globalUserId, 9999999999L);
+
+        refreshTokenRepository.saveWithAccessToken(currentRT1.getTokenString(), currentAT1.getTokenString());
+
+        TokenDto currentAT2 = jwtTokenService.generateToken(globalUserId, 1000000000L);
+        TokenDto currentRT2 = jwtTokenService.generateToken(globalUserId, 9999999999L);
+
+        refreshTokenRepository.saveWithAccessToken(currentRT2.getTokenString(), currentAT2.getTokenString());
+
+
+        ReissueTokenRequestDto body = new ReissueTokenRequestDto(
+                createTokenStringForResponse(currentAT1),
+                createTokenStringForResponse(currentRT2));
+
+        //when, then
+        mockMvc.perform(post("/v1/user/reissue-token")
+                        .content(objectMapper.writeValueAsString(body))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status.resCode").value(INVALID_TOKEN.getCode()));
     }
 
 }
