850be7ef6a10461cbe92108f18c98f7fc0dc34c8
JinuCheon <76773202+JinuCheon@users.noreply.github.com>
2023-02-18 16:55:48 +0900

    feat: 게시글 신고를 위한 엔티티 설계

---
+++ src/main/java/com/nexters/phochak/domain/ReportPost.java
@@ -0,0 +1,46 @@
+package com.nexters.phochak.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Getter
+@Entity
+public class ReportPost extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="REPORT_ID")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private User reporter;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "POST_ID", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Post post;
+
+    private String reason;
+
+    public ReportPost() {
+    }
+
+    @Builder
+    public ReportPost(Long id, User reporter, Post post, String reason) {
+        this.id = id;
+        this.reporter = reporter;
+        this.post = post;
+        this.reason = reason;
+    }
+}
