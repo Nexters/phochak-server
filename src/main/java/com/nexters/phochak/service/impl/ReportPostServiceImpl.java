9fd853bde9296a89307a3cc2e404617d02deabb2
JinuCheon <woojin8787@gmail.com>
2023-02-19 18:32:22 +0900

    style: 코드 스타일 수정

--- src/main/java/com/nexters/phochak/service/impl/ReportPostServiceImpl.java
+++ src/main/java/com/nexters/phochak/service/impl/ReportPostServiceImpl.java
@@ -15,8 +15,7 @@ import com.nexters.phochak.service.ReportPostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
