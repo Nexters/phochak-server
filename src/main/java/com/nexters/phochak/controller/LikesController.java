1ef793bc0a124331f2139110830025392e7df5d8
JinuCheon <76773202+JinuCheon@users.noreply.github.com>
2023-02-26 00:05:53 +0900

    refactor: 제약조건 exception 처리

--- src/main/java/com/nexters/phochak/controller/LikesController.java
+++ src/main/java/com/nexters/phochak/controller/LikesController.java
@@ -3,13 +3,18 @@ package com.nexters.phochak.controller;
 import com.nexters.phochak.auth.UserContext;
 import com.nexters.phochak.auth.annotation.Auth;
 import com.nexters.phochak.dto.response.CommonResponse;
+import com.nexters.phochak.exception.PhochakException;
+import com.nexters.phochak.exception.ResCode;
 import com.nexters.phochak.service.PhochakService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.ConstraintViolationException;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,7 +27,11 @@ public class LikesController {
     @PostMapping
     public CommonResponse<Void> addPhochak(@PathVariable Long postId) {
         Long userId = UserContext.getContext();
-        phochakService.addPhochak(userId, postId);
+        try {
+            phochakService.addPhochak(userId, postId);
+        } catch (ConstraintViolationException | DataIntegrityViolationException e) {
+            throw new PhochakException(ResCode.ALREADY_PHOCHAKED);
+        }
         return new CommonResponse<>();
     }
 
