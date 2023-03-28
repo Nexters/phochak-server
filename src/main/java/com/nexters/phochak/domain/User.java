0f69d293aeeb78a9960bb8c7660f2690d625ba27
JinuCheon <woojin8787@gmail.com>
2023-03-28 12:41:36 +0900

    feat: uploadKey로 deviceToken 토큰 조회

--- src/main/java/com/nexters/phochak/domain/User.java
+++ src/main/java/com/nexters/phochak/domain/User.java
@@ -10,9 +10,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
@@ -27,6 +30,9 @@ public class User extends BaseTime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "USER_ID")
     private Long id;
+
+    @OneToOne(mappedBy = "user")
+    private FcmDeviceToken fcmDeviceToken;
 
     @Enumerated(EnumType.STRING)
     private OAuthProviderEnum provider;
