2a0e12bf6496882b851d0680cfe721e787367480
seyeong <seyoung7555@naver.com>
2023-04-25 20:48:38 +0900

    feat: 유저 탈퇴 시 탈퇴일시 기록
    - 비즈니스 로직 작성
    - sql문 작성
    - 통합테스트 작성

--- src/test/java/com/nexters/phochak/integration/UserIntegrationTest.java
+++ src/test/java/com/nexters/phochak/integration/UserIntegrationTest.java
@@ -2,17 +2,24 @@ package com.nexters.phochak.integration;
 
 import com.nexters.phochak.domain.User;
 import com.nexters.phochak.repository.UserRepository;
+import com.nexters.phochak.service.PostService;
 import com.nexters.phochak.service.UserService;
 import com.nexters.phochak.specification.OAuthProviderEnum;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,24 +32,32 @@ class UserIntegrationTest {
 
     @Autowired
     UserService userService;
-
     @Autowired
     MockMvc mockMvc;
     @Autowired
     UserRepository userRepository;
+    @MockBean
+    PostService postService;
+
+    User user;
+
+    @BeforeEach
+    void setUp() {
+        String nickname = "testUser";
+
+        user = User.builder()
+                .id(1L)
+                .nickname(nickname)
+                .provider(OAuthProviderEnum.KAKAO)
+                .providerId("test")
+                .build();
+    }
 
     @Test
     @DisplayName("중복된 닉네임으로 가입하면 isDuplicated가 true로 반환된다")
     void duplicatedTest() throws Exception {
         // given
         String nickname = "testUser";
-
-        User user = User.builder()
-                .id(1L)
-                .nickname(nickname)
-                .provider(OAuthProviderEnum.KAKAO)
-                .providerId("test")
-                .build();
 
         userRepository.save(user);
 
@@ -51,5 +66,20 @@ class UserIntegrationTest {
                 .param("nickname", nickname))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data.isDuplicated").value(true));
+    }
+
+    @Test
+    @DisplayName("회원 탈퇴 시 탈퇴 일시가 기록된다")
+    void withdrawTest() {
+        // given
+        userRepository.save(user);
+
+        // when
+        userService.withdraw(user.getId());
+
+        // then
+        Optional<User> findUser = userRepository.findById(user.getId());
+        assertThat(findUser).isPresent();
+        assertThat(findUser.get().getLeaveDate()).isNotNull();
     }
 }
