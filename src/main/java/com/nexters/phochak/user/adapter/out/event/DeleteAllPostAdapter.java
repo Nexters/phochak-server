6a4dd4a094ec7708a1936e7a05bba4335c4e053e
JinuCheon <woojin8787@gmail.com>
2023-09-21 15:05:40 +0900

    2. 기존 adapter의 event publisher를 DeleteAllPostClient 로 분리

--- src/main/java/com/nexters/phochak/user/adapter/out/event/DeleteAllPostAdapter.java
+++ src/main/java/com/nexters/phochak/user/adapter/out/event/DeleteAllPostAdapter.java
@@ -3,17 +3,16 @@ package com.nexters.phochak.user.adapter.out.event;
 import com.nexters.phochak.user.application.port.out.DeleteAllPostPort;
 import com.nexters.phochak.user.domain.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class DeleteAllPostAdapter implements DeleteAllPostPort {
 
-    private final ApplicationEventPublisher applicationEventPublisher;
+    private final DeleteAllPostNetworkClient deleteAllPostNetworkClient;
 
     @Override
     public void deleteAllPostByUser(final User user) {
-        applicationEventPublisher.publishEvent(new DeleteAllPostEvent(user.getId()));
+        deleteAllPostNetworkClient.deleteAllPostByUser(user);
     }
 }
